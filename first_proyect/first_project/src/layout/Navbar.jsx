f57ab1fda28f484126421a600e039a46e3be8fab
EstebanOG <juanesteban199911@gmail.com>
2020-11-24 16:35:09 -0500

    Ejemplo modulos.css

--- first_proyect/first_project/src/layout/Navbar.jsx
+++ first_proyect/first_project/src/layout/Navbar.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
-import './Navbar.css'
+import styles from './Navbar.module.css'
 
 function Navbar() {
   return (
-    <nav>
-      <ul className="navbar-list">
-        <li className="navbar-item">
+    <nav className={styles.Navbar}>
+      <ul className={styles['navbar-list']}>
+        <li className={styles['navbar-item']}>
           <Link to="/">Home</Link>
         </li>
-        <li className="navbar-item">
+        <li className={styles['navbar-item']}>
           <Link to="/about">About</Link>
         </li>
-        <li className="navbar-item">
+        <li className={styles['navbar-item']}>
           <Link to="/contact">Contact</Link>
         </li>
       </ul>
