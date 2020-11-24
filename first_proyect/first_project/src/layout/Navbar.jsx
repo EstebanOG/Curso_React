c40696d183e41a7b4f813f47846acf88a820119f
EstebanOG <juanesteban199911@gmail.com>
2020-11-24 16:29:30 -0500

    Se añade Navbar.css

--- first_proyect/first_project/src/layout/Navbar.jsx
+++ first_proyect/first_project/src/layout/Navbar.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+
 import { Link } from 'react-router-dom';
+
+import './Navbar.css'
 
 function Navbar() {
   return (
     <nav>
-      <ul>
-        <li>
+      <ul className="navbar-list">
+        <li className="navbar-item">
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li className="navbar-item">
           <Link to="/about">About</Link>
         </li>
-        <li>
+        <li className="navbar-item">
           <Link to="/contact">Contact</Link>
         </li>
       </ul>
